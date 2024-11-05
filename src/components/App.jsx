621032653feae75a94bfbb18b0c4c3b8ed48e4b8
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-05 23:24:12 +0530

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import EmployeeForm from './EmployeeForm';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Employee Management System</h1>
+            <EmployeeForm />
+        </div>
+    );
+};
+
+export default App;
