9d1b34232fce5e2de976ce764fb7836d78411089
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-05 23:15:57 +0530

    Update EmployeeForm.jsx

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -1,28 +1,4 @@
-import React, { useState, useRef } from 'react';
 
-const EmployeeForm = ({ onAddEmployee }) => {
-  const nameRef = useRef(null);
-  const ageRef = useRef(null);
-  const addressRef = useRef(null);
-  const departmentRef = useRef(null);
-  const salaryRef = useRef(null);
-  const [isMarried, setIsMarried] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Create an employee object from form input values
-    const newEmployee = {
-      name: nameRef.current.value,
-      age: ageRef.current.value,
-      address: addressRef.current.value,
-      department: departmentRef.current.value,
-      salary: salaryRef.current.value,
-      isMarried: isMarried
-    };
-
-    // Pass the new employee data back to the parent component
-    onAddEmployee(newEmployee);
 
     // Reset the form
     e.target.reset();
