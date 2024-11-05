0f4152ad3e4dfd5ccdf713b3ffcbcd4f77ae6f91
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-05 23:16:10 +0530

    Update EmployeeForm.jsx

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -1,20 +1,3 @@
 
 
-    // Reset the form
-    e.target.reset();
-    setIsMarried(false);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name: </label>
-        <input type="text" ref={nameRef} required />
-      </div>
-      
     
-  
-  );
-};
-
-export default EmployeeForm;
